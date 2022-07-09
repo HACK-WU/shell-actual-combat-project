ad42e99a8eb86d0af29c4d5504029a2f936b477b
HACK-WU <2931580128@qq.com>
2022-07-09 20:56:58 +0800

    Nginx日志系统分析

--- nginx_log_analysis.sh
+++ nginx_log_analysis.sh
@@ -21,23 +21,46 @@ function changeToENG {
 		12月)	echo "Dec" ;;
 	esac
 }
-function daily_pv {
+function daily_pv {				#统计指定日期的PV量
 	echo -e "\033[33m"
-  	read -p "please input a  date (09/Jul/2020:10:12:07): "  date
+  	read -p "please input a or two  date (09/Jul/2020:10:12:07): "  dates
 	echo "------------------------------------------------------------"
 	echo -e "\033[0m"
 	local Year=$(date +"%Y")
 	local Mon=$(date +"%b" |grep "月" &>/dev/null  &&  changeToENG `date +"%b"`||echo `date +"%b"` )	#将月份改为英文缩写
 	local Day=$(date +"%d")
-	[ ${#date} -gt 0 ] && local date=$date || local date="$Day/$Mon/$Year"
-	local today=$date		#当日的日期
-	local result=$(grep "$today" $log_path)
-	IFS=$'\n'
-	for item in $result
+	local num=0
+	for item in $dates
 	do
-		echo "$item"
-		echo "---------------------------------------------------------------------------------------------------"
+		local date_array[++num]=$item
 	done
+	case $num in
+	0)						#默认状态，没有写入参数
+		local date="$Day/$Mon/$Year"
+        local count=$(grep "$date" $log_path|wc -l) 
+		echo "目前今日的PV量为： $count"
+	 	;;
+	1) 						#写入了一个参数
+		local date=$dates
+		local count=$(grep "$date" $log_path | wc -l)
+		echo "$date 这个时刻的访问量为： $count"
+		;;
+
+	2) 						#写入了两个参数
+		local start_date=${date_array[1]}
+		local end_date=${date_array[2]}
+		local count=$(awk '{str=$4; sub(/\[/,"",str); if(str<="'''$end_date'''" && str>="'''$start_date'''") print $0}' $log_path|wc -l) 		     	
+		echo "从$start_date到$end_date的PV量为： $count"	
+		;;	
+	*) 						#参数过多
+		echo "参数过多，最多支持两个!!!!"
+		sleep 1
+		return 1
+		;;
+	esac
+
+	echo "----------------------------------------------------------------------"
+	
 }
 
 daily_pv	
